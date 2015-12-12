cec4e725192c09359615ef8f0fddec041437a706
marcoamamani <marcoantonimamanidiaz645@gmail.com>
2015-12-12 15:15:29 +0100

    marc

---
+++ controllers/post.js
@@ -0,0 +1,19 @@
+//Registro de Contacto
+//verificar si es un contacto correcto
+//req = peticion
+//res = respuesta
+
+var models = require('../models/model.js');
+
+exports.registroPost = function (req, res){
+	var post = models.Posts.build({ nombres: "Nombres",
+								
+post: "post"
+});
+	
+</h>	res.render('registroPost', {titulo:'Registro de Post', post: post});
+}
+
+//escribir el codigo para devolver respuesta
+
+//Escribir el codigo para guardar los datos
---
+++ models/post.js
@@ -0,0 +1,6 @@
+//Escriba la estructura de la tabla dentro de las llaves
+module.exports= function(sequelize, DataTypes){
+	return sequelize.define('Post', {});
+	<laber for="apellido paterno"
+	class="rem"> apellido paterno: </label>
+}
